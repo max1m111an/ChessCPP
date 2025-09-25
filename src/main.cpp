cd810d6893e33ea6a15e12720533f799df24f5d3
max1m111an <maksimilianbegunov@inbox.ru>
2025-09-25 21:20:42 +0300

    src/main: set figures at board
    add moveFigures, fix drawFigures
    add defines to filenames
    add icon
    fps=60

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,172 @@
-#include <iostream>
-#include <string>
-
-#include "raylib.h"
-#include "figures.cpp"
-#include <unordered_map>
+#include "figures.h"
 
 typedef std::unordered_map<std::string, Texture2D> textureMap;
+#define filenameWP "../textures/wP.png"
+#define filenameWQ "../textures/wQ.png"
+#define filenameWK "../textures/wK.png"
+#define filenameWR "../textures/wR.png"
+#define filenameWN "../textures/wN.png"
+#define filenameWB "../textures/wB.png"
+#define filenameBP "../textures/bP.png"
+#define filenameBQ "../textures/bQ.png"
+#define filenameBK "../textures/bK.png"
+#define filenameBR "../textures/bR.png"
+#define filenameBN "../textures/bN.png"
+#define filenameBB "../textures/bB.png"
+#define filenameIcon "../textures/chesscpp_icon.png"
 
-const std::unordered_map<std::string, int> letterToNumber = {
-    {"a", 1},
-    {"b", 2},
-    {"c", 3},
-    {"d", 4},
-    {"e", 5},
-    {"f", 6},
-    {"g", 7},
-    {"h", 8},
+
+std::string pos[CELLS_QUANT][CELLS_QUANT] {
+    {"bR", "bN", "bB", "bK", "bQ", "bb", "bn", "br"},
+    {"bP0", "bP1", "bP2", "bP3", "bP4", "bP5", "bP6", "bP7"},
+    {"", "", "", "", "", "", "", ""},
+    {"", "", "", "", "", "", "", ""},
+    {"", "", "", "", "", "", "", ""},
+    {"", "", "", "", "", "", "", ""},
+    {"wP0", "wP1", "wP2", "wP3", "wP4", "wP5", "wP6", "wP7"},
+    {"wR", "wN", "wB", "wK", "wQ", "wb", "wn", "wr"}
 };
 
+std::unordered_map<std::string, FigureType> strToFigureType {
+    {"wK", KING},
+    {"wQ", QUEEN},
+    {"wR", ROOK},
+    {"wB", BISHOP},
+    {"wN", KNIGHT},
+    {"wP", PAWN},
+    {"bK", KING},
+    {"bQ", QUEEN},
+    {"bR", ROOK},
+    {"bB", BISHOP},
+    {"bN", KNIGHT},
+    {"bP", PAWN},
+};
+
+// Load figure textures dict
 textureMap LoadData() {
     textureMap vimrc = {
-        {"bB", LoadTexture("../textures/bB.png")},
-        {"bQ", LoadTexture("../textures/bQ.png")},
-        {"bK", LoadTexture("../textures/bK.png")},
-        {"bN", LoadTexture("../textures/bN.png")},
-        {"bR", LoadTexture("../textures/bR.png")},
-        {"bP", LoadTexture("../textures/bP.png")},
-        {"wB", LoadTexture("../textures/wB.png")},
-        {"wQ", LoadTexture("../textures/wQ.png")},
-        {"wK", LoadTexture("../textures/wK.png")},
-        {"wN", LoadTexture("../textures/wN.png")},
-        {"wR", LoadTexture("../textures/wR.png")},
-        {"wP", LoadTexture("../textures/wP.png")},
-    };
+            {"bb", LoadTexture(filenameBB)},
+            {"bB", LoadTexture(filenameBB)},
+            {"bQ", LoadTexture(filenameBQ)},
+            {"bK", LoadTexture(filenameBK)},
+            {"bN", LoadTexture(filenameBN)},
+            {"bn", LoadTexture(filenameBN)},
+            {"bR", LoadTexture(filenameBR)},
+            {"br", LoadTexture(filenameBR)},
+            {"bP0", LoadTexture(filenameBP)},
+            {"bP1", LoadTexture(filenameBP)},
+            {"bP2", LoadTexture(filenameBP)},
+            {"bP3", LoadTexture(filenameBP)},
+            {"bP4", LoadTexture(filenameBP)},
+            {"bP5", LoadTexture(filenameBP)},
+            {"bP6", LoadTexture(filenameBP)},
+            {"bP7", LoadTexture(filenameBP)},
+            {"wb", LoadTexture(filenameWB)},
+            {"wB", LoadTexture(filenameWB)},
+            {"wQ", LoadTexture(filenameWQ)},
+            {"wK", LoadTexture(filenameWK)},
+            {"wN", LoadTexture(filenameWN)},
+            {"wn", LoadTexture(filenameWN)},
+            {"wR", LoadTexture(filenameWR)},
+            {"wr", LoadTexture(filenameWR)},
+            {"wP0", LoadTexture(filenameWP)},
+            {"wP1", LoadTexture(filenameWP)},
+            {"wP2", LoadTexture(filenameWP)},
+            {"wP3", LoadTexture(filenameWP)},
+            {"wP4", LoadTexture(filenameWP)},
+            {"wP5", LoadTexture(filenameWP)},
+            {"wP6", LoadTexture(filenameWP)},
+            {"wP7", LoadTexture(filenameWP)}
+        };
     return vimrc;
 }
 
-void drawFigures(textureMap &t, const Board &_board) {
-    const double scaleFigureToCell = static_cast<double>(cellSize) / t["wK"].width;
+std::pair<int, int> getPosXY(const std::string &figureType) {
+    int i = figureType[0] == 'w' ? 6 : 0;
+    while (i < CELLS_QUANT) {
+        for (int j = 0; j < CELLS_QUANT; ++j) {
+            if (figureType == pos[i][j]) return {j, i};
+        }
+        ++i;
+    }
+    return {0, 0};
+}
 
+void drawFigures(const textureMap &t, Board &board) {
     for (const auto& figure: t) {
-        DrawTextureEx(figure.second,
-                  (Vector2){0, 0},
-                  0,
-                  static_cast<float>(scaleFigureToCell),
-                  WHITE);
+        double scaleFigureToCell = static_cast<double>(CELL_SIZE) / figure.second.width;
+        bool isFigureWhite = figure.first[0] == 'w';
+        std::pair<int, int> xy = getPosXY(figure.first);
+
+        Figure newFigure(strToFigureType[figure.first], figure.second, isFigureWhite,
+                         static_cast<float>(xy.first) * CELL_SIZE, static_cast<float>(xy.second) * CELL_SIZE);
+        board.board[xy.first][xy.second].figure = newFigure;
+
+        DrawTextureEx(newFigure.texture, (Vector2){newFigure.x, newFigure.y},
+                  0, static_cast<float>(scaleFigureToCell), WHITE);
+    }
+}
+
+inline std::pair<int, int> getMousePosXY(const Vector2 &mousePos) {
+    return {static_cast<int>(mousePos.x) / CELL_SIZE, static_cast<int>(mousePos.y) / CELL_SIZE};
+}
+
+void moveFigures(const Vector2 &mousePos, Board &board) {
+    std::pair<int, int> xy = getMousePosXY(mousePos);
+    if (board.board[xy.first][xy.second].occupied()) {
+        Figure thisFigure = board.board[xy.first][xy.second].figure.value();
+        thisFigure.moveFigure(mousePos.x, mousePos.y);
     }
 }
 
 int main() {
-    constexpr int cellsQuantity = 8;
-    constexpr int screenWidth = cellSize * cellsQuantity;
-    constexpr int screenHeight = screenWidth + 150;
+    constexpr int SCREEN_WIDTH {CELL_SIZE * CELLS_QUANT};
+    constexpr int SCREEN_HEIGHT {SCREEN_WIDTH + 150};
+    constexpr int FPS {60};
 
-    InitWindow(screenWidth, screenHeight, "ChessCPP");
+    Vector2 mousePosition {0};
 
-    std::unordered_map<std::string, Texture2D> guy = LoadData();
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "ChessCPP");
+    SetWindowIcon(LoadImage(filenameIcon));
+
+    textureMap guy = LoadData();
+
+    SetTargetFPS(FPS);
 
     // Main game loop
     while (!WindowShouldClose()) {
+
+        mousePosition = GetMousePosition();
+
         BeginDrawing();
         Board mainBoard;
 
-        for (int i = 0; i < cellsQuantity; i++) {
-            for (int j = 0; j < cellsQuantity; j++) {
+        for (int i = 0; i < CELLS_QUANT; i++) {
+            for (int j = 0; j < CELLS_QUANT; j++) {
                 // mainBoard.board[i][j] = mainBoard.board[i][j];
-                const Cell newCell((i + j) % 2 == 0);
+                Cell newCell((i + j) % 2 == 0);
                 mainBoard.board[i][j] = newCell;
                 DrawRectangle(
-                    i * cellSize,
-                    j * cellSize,
-                    cellSize,
-                    cellSize,
+                    i * CELL_SIZE,
+                    j * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE,
                     newCell.isBlack ? WHITE : BLACK);
             }
         }
 
         drawFigures(guy, mainBoard);
 
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            moveFigures(mousePosition, mainBoard);
+        }
+
+
         ClearBackground(RAYWHITE);
         EndDrawing();
     }
+
+    for (const auto& texture: guy) UnloadTexture(texture.second);
 
     CloseWindow();
 
