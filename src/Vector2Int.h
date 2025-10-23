d98ddd723b452784360000766ee53e2697639464
max1m111an <maksimilianbegunov@inbox.ru>
2025-10-24 01:25:53 +0300

    Make allowMoves real and visible

--- src/Vector2Int.h
+++ src/Vector2Int.h
@@ -6,7 +6,7 @@ struct Vector2Int {
     Vector2Int() : first(0), second(0) {}
     Vector2Int(const int first, const int second) : first(first), second(second) {}
     Vector2Int(const float first, const float second) : first(static_cast<int>(first)), second(static_cast<int>(second)) {}
-    bool operator==(const Vector2Int &) const = default;
+    bool operator==(const Vector2Int&) const = default;
 };
 
 #endif //VECTOR2INT_H
--- src/figures.cpp
+++ src/figures.cpp
@@ -48,3 +48,95 @@ FigureType Queen::getType() {
 FigureType King::getType() {
     return KING;
 }
+
+
+std::vector<Vector2Int> Figure::getAllowMoves() const {
+    return {};
+}
+
+
+std::vector<Vector2Int> Pawn::getAllowMoves() const {
+    const Vector2Int currentPos = getPosXYFloatToInt(x, y);
+    std::vector<Vector2Int> allowMoves;
+    allowMoves.reserve(2);
+    if (isWhite) {
+        allowMoves.emplace_back(currentPos.first - 1, currentPos.second - 1);
+        allowMoves.emplace_back(currentPos.first + 1, currentPos.second - 1);
+    } else {
+        allowMoves.emplace_back(currentPos.first - 1, currentPos.second + 1);
+        allowMoves.emplace_back(currentPos.first + 1, currentPos.second + 1);
+    }
+    return allowMoves;
+}
+
+
+std::vector<Vector2Int> Knight::getAllowMoves() const {
+    const Vector2Int currentPos = getPosXYFloatToInt(x, y);
+    std::vector<Vector2Int> allowMoves;
+    allowMoves.reserve(8);
+    for (int i = -1; i <= 1; i += 2) {
+        allowMoves.emplace_back(currentPos.first - 2, currentPos.second + i);
+        allowMoves.emplace_back(currentPos.first + 2, currentPos.second + i);
+        allowMoves.emplace_back(currentPos.first - 1, currentPos.second + 2 * i);
+        allowMoves.emplace_back(currentPos.first + 1, currentPos.second + 2 * i);
+    }
+    return allowMoves;
+}
+
+
+std::vector<Vector2Int> Bishop::getAllowMoves() const {
+    const Vector2Int currentPos = getPosXYFloatToInt(x, y);
+    std::vector<Vector2Int> allowMoves;
+    allowMoves.reserve(14);
+    for (int i = 0; i < CELLS_QUANT; ++i) {
+        for (int j = -1; j <= 1; j += 2) {
+            allowMoves.emplace_back(currentPos.first + i, currentPos.second - i * j);
+            allowMoves.emplace_back(currentPos.first - i, currentPos.second - i * j);
+        }
+    }
+    return allowMoves;
+}
+
+
+std::vector<Vector2Int> Rook::getAllowMoves() const {
+    const Vector2Int currentPos = getPosXYFloatToInt(x, y);
+    std::vector<Vector2Int> allowMoves;
+    allowMoves.reserve(14);
+    for (int i = 0; i < CELLS_QUANT; ++i) {
+        for (int j = -1; j <= 1; j += 2) {
+            allowMoves.emplace_back(currentPos.first, currentPos.second + i * j);
+            allowMoves.emplace_back(currentPos.first + i * j, currentPos.second);
+        }
+    }
+    return allowMoves;
+}
+
+
+std::vector<Vector2Int> Queen::getAllowMoves() const {
+    const Vector2Int currentPos = getPosXYFloatToInt(x, y);
+    std::vector<Vector2Int> allowMoves;
+    allowMoves.reserve(28);
+    for (int i = 0; i < CELLS_QUANT; ++i) {
+        for (int j = -1; j <= 1; j += 2) {
+            allowMoves.emplace_back(currentPos.first, currentPos.second + i * j);
+            allowMoves.emplace_back(currentPos.first + i * j, currentPos.second);
+            allowMoves.emplace_back(currentPos.first + i, currentPos.second - i * j);
+            allowMoves.emplace_back(currentPos.first - i, currentPos.second - i * j);
+        }
+    }
+    return allowMoves;
+}
+
+
+std::vector<Vector2Int> King::getAllowMoves() const {
+    const Vector2Int currentPos = getPosXYFloatToInt(x, y);
+    std::vector<Vector2Int> allowMoves;
+    allowMoves.reserve(8);
+    allowMoves.emplace_back(currentPos.first, currentPos.second - 1);
+    allowMoves.emplace_back(currentPos.first, currentPos.second + 1);
+    for (int i = -1; i <= 1; ++i) {
+        allowMoves.emplace_back(currentPos.first - 1, currentPos.second + i);
+        allowMoves.emplace_back(currentPos.first + 1, currentPos.second + i);
+    }
+    return allowMoves;
+}
